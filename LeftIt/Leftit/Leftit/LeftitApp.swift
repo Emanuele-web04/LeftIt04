7186916bd3eaffa6991a0f12840e1e8219a77926
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-03-22 17:34:19 +0100

    first

---
+++ LeftIt/Leftit/Leftit/LeftitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftitApp.swift
+//  Leftit
+//
+//  Created by Emanuele Di Pietro on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LeftitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
